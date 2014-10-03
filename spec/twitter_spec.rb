147cdfd5350b93c264506e308a832eaa2af5cb3f
Ian Miller <irmiller22@gmail.com>
2014-10-03 10:06:37 -0400

    updated spec

--- spec/twitter_spec.rb
+++ spec/twitter_spec.rb
@@ -3,6 +3,8 @@ require 'pry'
 
 describe TwitterApi do
   let(:client) { TwitterApi.new }
+  # BE SURE TO ADD YOUR USERNAME INSIDE OF THE STRING BELOW!
+  let(:user) { "<USERNAME>" }
 
   before(:all) do
     encoded = "cmVxdWlyZSAndHdpdHRlcicKcmVxdWlyZSAneWFtbCcKCmNsYXNzIFNwZWNU\nd2l0dGVyQXBpCiAgYXR0cl9yZWFkZXIgOmNsaWVudAoKICBkZWYgaW5pdGlh\nbGl6ZQogICAga2V5cyA9IFlBTUwubG9hZF9maWxlKCdhcHBsaWNhdGlvbi55\nbWwnKQoKICAgIEBjbGllbnQgPSBUd2l0dGVyOjpSRVNUOjpDbGllbnQubmV3\nIGRvIHxjb25maWd8CiAgICAgIGNvbmZpZy5jb25zdW1lcl9rZXkgICAgICAg\nID0ga2V5c1snQ09OU1VNRVJfS0VZJ10KICAgICAgY29uZmlnLmNvbnN1bWVy\nX3NlY3JldCAgICAgPSBrZXlzWydDT05TVU1FUl9TRUNSRVQnXQogICAgICBj\nb25maWcuYWNjZXNzX3Rva2VuICAgICAgICA9IGtleXNbJ0FDQ0VTU19UT0tF\nTiddCiAgICAgIGNvbmZpZy5hY2Nlc3NfdG9rZW5fc2VjcmV0ID0ga2V5c1sn\nQUNDRVNTX1RPS0VOX1NFQ1JFVCddCiAgICBlbmQKICBlbmQKCiAgZGVmIG1v\nc3RfcmVjZW50X2ZvbGxvd2VyCiAgICBjbGllbnQuZnJpZW5kcy5maXJzdAog\nIGVuZAoKICBkZWYgZmluZF91c2VyX2Zvcih1c2VybmFtZSkKICAgIGNsaWVu\ndC51c2VyKHVzZXJuYW1lKQogIGVuZAoKICBkZWYgZmluZF9mb2xsb3dlcnNf\nZm9yKHVzZXIpCiAgICBjbGllbnQuZm9sbG93ZXJzKHVzZXIpLnRha2UoMTAp\nCiAgZW5kCmVuZAo=\n"
@@ -27,13 +29,13 @@ describe TwitterApi do
 
   describe "#find_user_for" do
     it 'returns the Twitter user associated with username that you passed in as a parameter' do
-      expect( client.find_user_for("irmiller22") ).to eq( SpecTwitterApi.new.find_user_for("irmiller22") )
+      expect( client.find_user_for(user) ).to eq( SpecTwitterApi.new.find_user_for(user) )
     end
   end
 
   describe "#find_followers_for" do
     it 'returns 10 followers for a specific Twitter user' do
-      expect( client.find_followers_for('irmiller22') ).to eq( SpecTwitterApi.new.find_followers_for('irmiller22') )
+      expect( client.find_followers_for(user) ).to eq( SpecTwitterApi.new.find_followers_for(user) )
     end
   end
 end
