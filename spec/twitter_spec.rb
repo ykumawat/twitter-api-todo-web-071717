5407e6de6c5aed45691228fb51005299863e86b9
msuzoagu <msuzoagu@gmail.com>
2015-08-12 09:09:49 -0400

    add missing ends

--- spec/twitter_spec.rb
+++ spec/twitter_spec.rb
@@ -2,10 +2,10 @@
  require 'pry'
  require 'vcr'
 
-describe TwitterApi do 
+describe TwitterApi do
   let(:client) {TwitterApi.new}
 
-  describe "#most_recent_friend" do 
+  describe "#most_recent_friend" do
     it "returns the most recent friend" do
       VCR.use_cassette('twitter/most_recent_friend') do
         user = client.most_recent_friend
@@ -15,30 +15,32 @@ describe TwitterApi do
     end
   end
 
-  describe "#find_user_for" do 
-    it "given a username, it returns the user object" do 
+  describe "#find_user_for" do
+    it "given a username, it returns the user object" do
       VCR.use_cassette('twitter/find_user_for') do
         user = client.find_user_for("sm_debenedetto")
         expect(user.class).to eq(Twitter::User)
-        expect(user.username).to eq("sm_debenedetto") 
+        expect(user.username).to eq("sm_debenedetto")
       end
     end
   end
 
-  describe "#find_followers_for" do 
-    it "given a username, it returns that user's followers" do 
+  describe "#find_followers_for" do
+    it "given a username, it returns that user's followers" do
       VCR.use_cassette('twitter/find_followers_for') do
         user = client.find_followers_for("sm_debenedetto")
-        expect(user).to be_a(Array) 
+        expect(user).to be_a(Array)
         expect(user.first.class).to eq(Twitter::User)
         expect(user.first.name).to eq("Adeline Gross")
       end
+    end
+  end
 
-  describe "#homepage_timeline" do 
-    it "returns an array of tweet objects from the client user's homepage" do 
+  describe "#homepage_timeline" do
+    it "returns an array of tweet objects from the client user's homepage" do
       VCR.use_cassette('twitter/homepage_timeline') do
         tweet = client.homepage_timeline
-        expect(tweet).to be_a(Array) 
+        expect(tweet).to be_a(Array)
         expect(tweet.first.class).to eq(Twitter::Tweet)
         expect(tweet.first.id).to eq(629359789647265792)
       end
